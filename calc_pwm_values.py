b6ecee5099d01bfe6ff33fa99cb3532f27e27ffd
Menchik <allan.m@ufms.br>
2024-07-17 09:55:19 -0400

    calc pwn values

---
+++ calc_pwm_values.py
@@ -0,0 +1,16 @@
+# Código que calcula o valor de Counter Period para o PWM do microcontrolador STM32F103C8T6
+# dado um valor de frequência desejada e um valor de prescaler a ser utilizado.
+
+freq_desejada = int(input("Digite a frequência desejada: "))
+
+period_desejado = 1/freq_desejada
+
+prescaler = int(input("Digite o valor do prescaler: "))
+
+freq = 72000000 / prescaler
+
+period = 1/freq
+
+counter_period = period_desejado/period
+
+print("Counter Period: ", round(counter_period))
